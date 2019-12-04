edb2874db37d52c0faf9906c67771caf52b4e9c7
Ian Kimble <ian.kimble@eRT.com>
2019-12-03 20:59:25 -0500

    starting frontend

---
+++ client/src/MainRouter.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Route, Switch } from "";
+import Home from "";
+import Menu from "";
+import Signup from "";
+import Signin from "";
+import MyCalander from "";
+import Profile from "";
+
+const MainRouter = () => (
+  <div>
+    <h2>Test</h2>
+  </div>
+);
+export default MainRouter;
---
+++ client/src/core/Menu.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { Link, withRouter } from "";
+import { signout, isAuthenticated } from "";
+
+const Home = () => <div>Welcome to Myday!</div>;
+export default Home;
